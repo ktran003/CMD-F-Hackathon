21044c04a8916fa9a9c2e51d8b145461b82821ca
stahsin00 <lamisa1998@hotmail.com>
2023-03-12 11:41:21 -0700

    more css

--- my-app/src/pages/Home.js
+++ my-app/src/pages/Home.js
@@ -6,21 +6,42 @@ import './Home.css';
 
  export const Home = () => {
     return (
-    <div class="home">
-        <div class="rectangle"></div>
-        <ul>
+    <div className="home">
+        <div className="titleArea">
+            <h1> 4 hours 30 minutes</h1>
+            <p>spent studying on Pomo's Paradise</p>
+        </div>
+        <div className="timerArea">
             <button class="timerButton">CHECK TIMER</button>
-        </ul>
-        <Link to="/studysets">
-         <button class="moreButton">
-            VIEW MORE
-         </button>
-        </Link>
-        <Link to="/chatbox">
-         <button>
-            STUDY WITH POMO
-         </button>
-        </Link>
+        </div>
+        <div className="studyArea">
+            <h3>my study sets</h3>
+            <hr></hr>
+            <pre>name     class      date creates</pre>
+            <pre>name     class      date creates</pre>
+            <pre>name     class      date creates</pre>
+            <pre>name     class      date creates</pre>
+            <pre>name     class      date creates</pre>
+            <pre>name     class      date creates</pre>
+            <Link to="/studysets">
+            <button class="moreButton">
+                VIEW MORE
+            </button>
+            </Link>
+        </div>
+        <hr></hr>
+        <div className="chatArea">
+            <Link to="/chatbox">
+            <button>
+                STUDY WITH POMO
+            </button>
+            </Link>
+            <Link to="/chatbox">
+            <button className="bigButton">
+                &gt;
+            </button>
+            </Link>
+        </div>
     </div>
     )
 }
