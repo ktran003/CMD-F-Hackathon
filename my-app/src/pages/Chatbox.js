a2c01c53977bbaa3b0a6f6cc3a73c287ea558dd1
kennedyyung <kennedyyung19@gmail.com>
2023-03-12 11:39:38 -0700

    finished chatbox

--- my-app/src/pages/Chatbox.js
+++ my-app/src/pages/Chatbox.js
@@ -1,5 +1,43 @@
 import React from "react"
+import './Chatbox.css'
 
 export default function Chatbox() {
-    return <h1>this page is still under construction</h1>
+    return (
+    <div className="className">
+        <div className="POMO">
+        <h3>POMO</h3>
+        <ul className= "pomotext">
+            <p>Hi! I'm Pomo. Lets start studying</p>
+            <p>Please enter what study set you'd like to study</p>
+        </ul>
+        </div>
+        <ul className="user">
+            <h3>ME</h3>
+            <p>Functions of the heart</p>
+        </ul>
+        <div className="POMO">
+        <h3>POMO</h3>
+        <ul className= "pomotext">
+            <p>How many chambers does the heart have?</p>
+
+        </ul>
+        </div>
+            <ul className="user">
+                <h3>ME</h3>
+                <p1>4</p1>
+            </ul>
+                    <div className="POMO">
+        <h3>POMO</h3>
+        <ul className= "pomotext">
+            <p1>Answer: 4</p1>
+
+        </ul>
+        </div>
+
+        <div className="container">
+        <ul><input className= "textBox" type='text'></input></ul>
+        <button>Enter</button>
+        </div>
+    </div>
+    )
 }
