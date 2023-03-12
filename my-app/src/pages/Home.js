1874971da20694b55200a7edde17a069d43c643a
kennedyyung <kennedyyung19@gmail.com>
2023-03-11 23:30:26 -0800

    adding router

--- my-app/src/pages/Home.js
+++ my-app/src/pages/Home.js
@@ -1,3 +1,24 @@
-export default function Home() {
-    return <h1>Home</h1>
+import TextSummarizer from "./TextSummarizer"
+
+ export default function Home() {
+    // let Component 
+    // switch (window.location.pathname) {
+    //   case "/textsummarizer":
+    //     Component = TextSummarizer
+    //     break
+    // }
+    return 
+    <div>
+    <h1>Home</h1>
+        {/* <CustomLink href="/textsummarizer">HOME</CustomLink> */}
+    </div>
 }
+
+// function CustomLink ({href, children, ...props}) {
+//     const path = window.location.pathname
+//     return (
+//     <li className= {path === href ? "active" : ""}>
+//         <a href={href} {...props}>{children}</a>
+//     </li>
+//     )
+// }
---
+++ my-app/src/pages/TextSummarizer.js
@@ -0,0 +1,9 @@
+// import UploadText from './Components/UploadText';
+
+export default function TextSummarizer() {
+    return 
+    // <>
+    // <h1>TextSummarizer</h1>
+    // <UploadText/>
+    // </>
+}
